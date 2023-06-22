8f72df12794922be7e22a37b4e79ff310cf2bf43
davidruc <davidarueda18@gmail.com>
2023-06-22 15:19:06 -0500

    feat: :sparkles: CRUD de maint_area
    Básicamente el crud es el mismo de todos los _area y hace falta hacer los JOINS para que las consultas funcionen bien

---
+++ scripts/maint_area/maint_area.php
@@ -0,0 +1,78 @@
+<?php
+
+class maint_area extends connect{
+    private $queryPost = 'INSERT INTO maint_area(id, id_area, id_staff, id_position, id_journeys) VALUES (:identificador, :fk_area, :fk_staff, :fk_posicion, :fk_journeys)';
+    private $queryGetAll = 'SELECT id AS "identificador", id_area AS "fk_area", id_staff AS "fk_staff", id_position AS "fk_posicion", id_journeys AS "fk_journeys" FROM maint_area';
+    private $queryUpdate = 'UPDATE maint_area SET id_area = :fk_area, id_staff = :fk_staff, id_position = :fk_posicion, id_journeys = :fk_journeys WHERE id = :identificador';
+    private $queryDelete = 'DELETE FROM maint_area WHERE id = :identificador';
+    private $message;
+    use getInstance;
+
+    function __construct(private $id=1, private $id_area=1, private $id_staff=1, private $id_position=1, private $id_journeys=1 ){
+        parent::__construct();
+    }
+
+    public function postMaintArea(){
+        try{
+            $res = $this->conexion->prepare($this->queryPost);
+            $res->bindValue("identificador", $this->id);
+            $res->bindValue("fk_area", $this->id_area);
+            $res->bindValue("fk_staff", $this->id_staff);
+            $res->bindValue("fk_posicion", $this->id_position);
+            $res->bindValue("fk_journeys", $this->id_journeys);
+            $res->execute();
+            $this->message = ["Code" => 200 + $res->rowCount(), "Message"=>"inserted Data"];
+        } catch (\PDOException $e){
+            $this->message = ["Code" => $e->getCode(), "Message"=> $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+     
+    }
+    public function getAllMaintAreax(){
+        try{
+            $res = $this->conexion->prepare($this->queryGetAll);
+            $res->execute();
+            $this->message = ["Code" => 200, "Message" => $res->fetchAll(PDO::FETCH_ASSOC)];
+        }   catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        }   finally {
+            print_r($this->message);
+        }
+    }
+    public function updateMaintAreax(){
+        try{
+            $res = $this->conexion->prepare($this->queryUpdate);
+            $res->bindValue("identificador", $this->id);
+            $res->bindValue("fk_area", $this->id_area);
+            $res->bindValue("fk_staff", $this->id_staff);
+            $res->bindValue("fk_posicion", $this->id_position);
+            $res->bindValue("fk_journeys", $this->id_journeys);
+            $res->execute();
+ 
+            if ($res->rowCount() > 0){
+                $this->message = ["Code" => 200, "Message" => "Data updated"];
+            }
+        } catch (\PDOException $e){
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function deleteMaintAreax(){
+        try{
+            $res = $this->conexion->prepare($this->queryDelete);
+            $res->bindValue("identificador", $this->id);
+            $res-> execute();
+            $this->message = ["Code" => 200, "Message" => "Data delete"];
+        } catch (\PDOException $e){
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+}
+
+
+
+?>

7f0ef56f4a548ce56a6f6030ecfcf3c5a515d916
davidruc <davidarueda18@gmail.com>
2023-06-25 15:56:44 -0500

    feat: :construction: Se agregaron las credenciales locales para trabajar el proyecto localmente

--- scripts/db/credentials.php
+++ scripts/db/credentials.php
@@ -1,10 +1,13 @@
 <?php 
     namespace App;
     abstract class credentials{   
-        protected $host = '172.16.49.20';
-        private $user = 'sputnik';
-        private $password = 'Sp3tn1kC@';
+        // protected $host = '172.16.49.20';
+        // private $user = 'sputnik';
+        // private $password = 'Sp3tn1kC@';
         protected $dbname = 'campusland';
+        protected $host = 'localhost';
+        private $user = 'root';
+        private $password = '';
         public function __get($name){
             return $this->{$name};
         }
